96f514043250cff6e8e2fbe87198ec672740e80b
illuz <iilluzen@gmail.com>
2023-02-07 23:23:18 +0800

    优化 errorCollection，student.js

--- app/service/student.js
+++ app/service/student.js
@@ -3,14 +3,23 @@
 
 // ========================================常用 require start===========================================
 const Service = require('egg').Service;
+const validateUtil = require("@jianghujs/jianghu/app/common/vaidateUtil");
 const dayjs = require("dayjs");
-const nanoid = require("nanoid");
+const { nanoid } = require("nanoid");
+
+const actionDataScheme = Object.freeze({
+  selectStudentList: {
+    type: "object",
+    additionalProperties: true,
+    required: [],
+    properties: {}
+  },
+});
 
 class StudentService extends Service {
+  // 生成学生 studentId
   async beforHookForGenerateStudentId() {
-    // 验证请求参数
     const { actionData } = this.ctx.request.body.appData;
-    // 逻辑处理
     const { dateOfBirth } = actionData;
     const dateOfBirthObj = dayjs(dateOfBirth);
     const studentId = `S_${nanoid.nanoid(8)}_${dateOfBirthObj.month()}_${dateOfBirthObj.day()}`;
@@ -18,6 +27,7 @@ class StudentService extends Service {
     this.ctx.request.body.appData.actionData.studentId = studentId;
   }
 
+  // beforeHook：将学生信息加到 ctx.userInfo 中
   async appendStudentInfoToUserInfo() {
     const studentInfo = await this.app
       .jianghuKnex("student")
@@ -26,7 +36,14 @@ class StudentService extends Service {
     this.ctx.userInfo.studentInfo = studentInfo || { classId: null };
   }
 
+  // 获取同班学生列表
   async selectStudentList() {
+
+    validateUtil.validate(
+      actionDataScheme.selectStudentList,
+      actionData
+    );
+
     const studentInfo = await this.app
       .jianghuKnex("student")
       .where({ studentId: this.ctx.userInfo.user.userId })
@@ -37,7 +54,7 @@ class StudentService extends Service {
       .select();
 
     return {
-      rows: studentList,
+      rows: studentList
     };
   }
 }
