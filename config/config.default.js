ade164a21a28904d0c8ebd7fe7c79c901513b9cf
colin <colin>
2024-04-12 21:22:54 +0800

    keys

--- config/config.default.js
+++ config/config.default.js
@@ -15,7 +15,7 @@ module.exports = appInfo => {
     appId,
     appTitle: '江湖演示-基础',
     appLogo: `${appId}/public/img/logo.svg`,
-    keys: `${appId}_1638108566009`,
+    keys: '1638108566009_jianghu',
 
     indexPage: `/${appId}/page/doUiAction`,
     loginPage: `/${appId}/page/login`,
